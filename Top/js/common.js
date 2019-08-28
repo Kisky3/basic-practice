0439fe894c1b9e34cdd61d209246e8b80f6caea3
XiangYu <kisky3@126.com>
2019-08-28 20:28:15 +0900

    :sparkle: slider response completed

--- Top/js/common.js
+++ Top/js/common.js
@@ -1,19 +1,22 @@
-  
 /* navigation bar animation control */
-window.onscroll = function () {    
-  var top = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;    
-  var node = document.getElementById('topbar');    
-  if (top > 400) {      
-    node.style.display = 'block';    
-  } else {      
-    node.style.display = 'none';    
-  }  
+window.onscroll = function() {
+    var top = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    var node = document.getElementById('topbar');
+    if (top > 400) {
+        node.style.display = 'block';
+    } else {
+        node.style.display = 'none';
+    }
 }
 
 /* Customer's Voice swiper control */
-var mySwiper = new Swiper ('.swiper-container', {
-  loop: true,
-  pagination: '.swiper-pagination',
-  nextButton: '.swiper-button-next',
-  prevButton: '.swiper-button-prev',
+var mySwiper = new Swiper('.swiper-container', {
+    type: 'bullets',
+    loop: true,
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    speed: 1000,
+    autoplay: false,
+    spaceBetween: 10
 })
