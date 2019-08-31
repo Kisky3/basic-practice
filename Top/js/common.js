b1df690c65bcac35ca23913e2517de6c1f5e542f
XiangYu <kisky3@126.com>
2019-08-31 18:35:23 +0900

    :+1: PC版完了　

--- Top/js/common.js
+++ Top/js/common.js
@@ -50,20 +50,70 @@ $('span[id^="dropcross"]').click(function() {
     $(this).attr('class', 'drop_cross_close'): $(this).attr('class', 'drop_cross_open');
 });
 
+function createModalImage(path) {
+    $('#modal_image').attr('src', path);
+    $('#contact_modal').show();
+}
 
-/*
+function createFormModalImage(path) {
+    $('#form_modal_img').attr('src', path);
+}
+
 window.onload = function() {
-    var modal = document.getElementById("mask");
-    var line = document.getElementById("line");
-    var bt = document.getElementById("bt");
-    var btclose = document.getElementById("btclose");
+    /* side bar contact modal */
+    /* modal image path */
+    let path;
 
-    bt.onclick = function() {
-        zhezhao.style.display = "block";
-        login.style.display = "block";
-    }
-    btclose.onclick = function() {
-        zhezhao.style.display = "none";
-        login.style.display = "none";
-    }
-}*/
+    $('#line').click(function() {
+        path = './img/contact_line.svg';
+        createModalImage(path);
+    });
+
+    $('#whatsapp').click(function() {
+        path = './img/contact_whatsapp.svg';
+        createModalImage(path);
+    });
+
+    $('#wechat').click(function() {
+        path = './img/contact_wechat.svg';
+        createModalImage(path);
+    });
+
+    $('#call').click(function() {
+        path = './img/contact_call.svg';
+        createModalImage(path);
+    });
+
+    /* contact modal */
+    $('#contact_form_btn').click(function() {
+        $('#contact_form_modal').show();
+    })
+
+    $('#contact_form_line').click(function() {
+        path = './img/contact_line.svg';
+        createFormModalImage(path)
+    })
+
+    $('#contact_form_whatsapp').click(function() {
+        path = './img/contact_whatsapp.svg';
+        createFormModalImage(path)
+    })
+
+    $('#contact_form_wechat').click(function() {
+        path = './img/contact_wechat.svg';
+        createFormModalImage(path)
+    })
+
+    $('#contact_form_call').click(function() {
+        path = './img/contact_call.svg';
+        createFormModalImage(path)
+    })
+
+    $('#modal_close_btn').click(function() {
+        $('#contact_modal').hide();
+    });
+
+    $('#form_moda_close_btn').click(function() {
+        $('#contact_form_modal').hide();
+    });
+}
