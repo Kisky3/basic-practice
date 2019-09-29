4e327861e7aa58372e4cfb50c916bcf777342e1e
XiangYu <kisky3@126.com>
2019-09-29 22:28:18 +0900

    Updates

---
+++ precache-manifest.7a8702519e594c8d5c4fa18e183f9862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e9d3abe9d071fc01abbbe65054e04d",
+    "url": "/Jirengu/todoList/hello-world/index.html"
+  },
+  {
+    "revision": "e06199bb381082f0af8b",
+    "url": "/Jirengu/todoList/hello-world/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7e81c3d6a8ca564314f1",
+    "url": "/Jirengu/todoList/hello-world/static/js/2.717ae7a0.chunk.js"
+  },
+  {
+    "revision": "e06199bb381082f0af8b",
+    "url": "/Jirengu/todoList/hello-world/static/js/main.280d7efd.chunk.js"
+  },
+  {
+    "revision": "0e1d981bd40774d44c11",
+    "url": "/Jirengu/todoList/hello-world/static/js/runtime-main.255f6856.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Jirengu/todoList/hello-world/static/media/logo.25bf045c.svg"
+  }
+]);
